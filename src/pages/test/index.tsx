59b509058ba87bf7e4068d0ed9db17df981c2102
Albert Boehmler <ajboehmler@gmail.com>
2022-06-12 16:18:43 -0400

    Some more notes

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -12,7 +12,11 @@ export default function Home(): JSX.Element {
     years: 5
   }
 
+  // Something happens in the Person.toBinary() call that requires a constructor
+  // in prod mode, but not in dev mode.
+  console.log("ABOUT TO CALL .toBinary()");
   const pb = Person.toBinary(p);
+  console.log("DONE CALLING .toBinary()");
   console.log(pb);
 
 
